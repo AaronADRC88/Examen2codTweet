8294557deed8da4b5644bdb943fcb9b2cd23fd77
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2015-03-16 09:01:24 +0100

    falta el acceso ánonimo.

---
+++ src/examensegundacodtwitter/ExamenSegundaCODTwitter.java
@@ -0,0 +1,26 @@
+package examensegundacodtwitter;
+
+import javax.swing.JOptionPane;
+import twitter4j.TwitterException;
+
+public class ExamenSegundaCODTwitter {
+
+    public static void main(String[] args) throws TwitterException {
+        /**
+         * instancia objeto tipo Metodos() Ventana para acceder a metodos
+         */
+        Metodos tweet = new Metodos();
+        int option = JOptionPane.showOptionDialog(null, "selecciona opcion", "menu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new Object[]{"Mostrar TimeLine", "Buscar en Twitter", "Publica un tweet", "Salir"}, "Salir");
+        if (option == 0) {
+            tweet.timeLine();
+        }
+        if (option == 1) {
+            tweet.buscar();
+        }
+        if (option == 2) {
+            tweet.tweet();
+        } else if (option != 0 && option != 1 && option != 2) {
+            System.exit(0);
+        }
+    }
+}
---
+++ src/examensegundacodtwitter/Metodos.java
@@ -0,0 +1,81 @@
+package examensegundacodtwitter;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ *
+ * @author aFerreiraDominguez
+ */
+public class Metodos {
+
+    ConfigurationBuilder cb = new ConfigurationBuilder();
+    Twitter twitter;
+
+    /**
+     * Aqui se introducen los codigos proporcionados en dev.twitter.com
+     */
+    public Metodos() {
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("ConsumerKey")
+                .setOAuthConsumerSecret("ConsumerSecret")
+                .setOAuthAccessToken("AccessToken")
+                .setOAuthAccessTokenSecret("AccessTokenSecret");
+        twitter = new TwitterFactory(cb.build()).getInstance();
+    }
+
+    /**
+     * Muestra timeLine
+     */
+    public void timeLine() {
+        try {
+            List<Status> statuses = twitter.getHomeTimeline();
+            System.out.println("Showing home timeline.");
+            for (Status status : statuses) {
+                System.out.println(status.getUser().getName() + ":"
+                        + status.getText());
+            }
+        } catch (TwitterException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Busca tweets con el texto introducido
+     */
+    public void buscar() {
+        try {
+            String preg = JOptionPane.showInputDialog("Buscar:");
+            Query query = new Query(preg);
+            QueryResult result = twitter.search(query);
+            for (Status status : result.getTweets()) {
+                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+            }
+        } catch (TwitterException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Crea y publica un tweet
+     */
+    public void tweet() {
+        try {
+            String twet = JOptionPane.showInputDialog("Tweett:");
+            Status status = twitter.updateStatus(twet);
+            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        } catch (TwitterException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
